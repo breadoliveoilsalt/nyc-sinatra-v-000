ad6f8ba39d1352c8a936191399d7c61d69199e9f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-05 15:00:32 -0500

    First seven tests passing

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,33 @@
+class FiguresController < ApplicationController
+
+  get '/figures/new' do
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+
+  #  binding.pry
+    @figure = Figure.create(params[:figure])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.find_or_create_by(name: params[:title][:name])
+    end
+
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures' do
+    erb :'figures/index'
+  end
+
+end
