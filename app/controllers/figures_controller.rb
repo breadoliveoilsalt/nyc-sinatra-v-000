bc1914ac3e777a17f2e7f4f77e8f60f46ae6c983
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-05 15:25:52 -0500

    figures controller tests passing

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -30,4 +30,25 @@ class FiguresController < ApplicationController
     erb :'figures/index'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.find_or_create_by(name: params[:title][:name])
+    end
+
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
 end
